fadaf50712126734b15de23d7a5a1beff1b2be87
Marc Gilligan <marcg@ulfnic.com>
2023-12-12 07:00:10 +0000

    feat: accept normal files and file descriptors for read/write

--- markdown2html.bash
+++ markdown2html.bash
@@ -24,18 +24,38 @@ print_stderr() {
 
 
 
+in_path=
+out_path='/dev/fd/1'
 [[ $1 ]] || help_doc 0
 while [[ $1 ]]; do
 	case $1 in
+		'-O')
+			shift; [[ $1 == '-' ]] && out_path='/dev/fd/1' || out_path=$1
+			;;
 		'--help'|'-h')
 			help_doc 0 ;;
 		'--')
 			shift; break ;;
-		'-'*)
+		'-'?*)
 			print_stderr 1 '%s\n' 'unrecognized parameter: '"$1" ;;
+		*)
+			[[ $1 == '-' ]] && in_path='/dev/fd/0' || in_path=$1 ;;
 	esac
 	shift
 done
+[[ $1 ]] && in_path=$1
+[[ $2 ]] && print_stderr 1 '%s\n' 'unrecognized parameter: '"$2"
+
+
+
+# Validate and convert param values
+[[ $in_path == '/dev/fd/'* ]] || [[ -f $in_path ]] || print_stderr 1 '%s\n' 'bad input filepath: '"$in_path"
+[[ $out_path == '/dev/fd/'* ]] || [[ -f $out_path ]] || print_stderr 1 '%s\n' 'bad output filepath: '"$in_path"
+
+
+
+# Read input file
+readarray -t line_arr < "$in_path"
 
 
 
